7c5baab14a70995e0eba04607a41fe37fc9e517c
Lucas <24826124+Luro02@users.noreply.github.com>
2022-10-28 07:38:57 +0200

    use `TypeUnion` struct for parsing matches

--- type-union-derive/src/input/type_union_match.rs
+++ type-union-derive/src/input/type_union_match.rs
@@ -2,9 +2,10 @@ use proc_macro2::{Ident, TokenStream};
 use quote::{quote, ToTokens, TokenStreamExt};
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
-use syn::{braced, parenthesized, Expr, Token, Type};
+use syn::{braced, Expr, Token, Type};
 
-use crate::utils::{assert_ident_is_type, resolve_type_union_name};
+use crate::input::TypeUnion;
+use crate::utils::assert_ident_is_type;
 
 #[derive(Debug, Clone)]
 pub struct TypeUnionMatchArm {
@@ -56,8 +57,7 @@ pub struct TypeUnionMatch {
     ident: Ident,
     _colon_token: Token![:],
     _borrow_token: Option<Token![&]>,
-    _paren_token: syn::token::Paren,
-    types: Punctuated<Type, Token![|]>,
+    type_union: TypeUnion,
     _brace_token: syn::token::Brace,
     arms: Punctuated<TypeUnionMatchArm, Token![,]>,
 }
@@ -65,13 +65,16 @@ pub struct TypeUnionMatch {
 impl Parse for TypeUnionMatch {
     fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
         let content;
-        let other_content;
         Ok(Self {
             ident: input.parse()?,
             _colon_token: input.parse()?,
             _borrow_token: input.parse()?,
-            _paren_token: parenthesized!(other_content in input),
-            types: other_content.parse_terminated(Type::parse)?,
+            type_union: {
+                let result: TypeUnion = input.parse()?;
+                // ensure that the TypeUnion is wrapped in `()`
+                result.has_parens_or_err()?;
+                result
+            },
             _brace_token: braced!(content in input),
             arms: content.parse_terminated(TypeUnionMatchArm::parse)?,
         })
@@ -83,7 +86,7 @@ impl ToTokens for TypeUnionMatch {
         let ident = &self.ident;
         let arms = self.arms.iter();
 
-        let real_type = resolve_type_union_name(self.types.iter());
+        let real_type = self.type_union.ident();
         let ty_path = Type::Path(syn::parse2(quote!(#real_type)).unwrap());
         let assertion = assert_ident_is_type(ident, &{
             if self._borrow_token.is_some() {
